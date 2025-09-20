d6261d599a0d4f94b26214c02512888b11a60f6b
naim-haider <naimhaider76@gmail.com>
2025-09-20 21:12:34 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { NewAppScreen } from '@react-native/new-app-screen';
+import { StatusBar, Text, useColorScheme, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+function App() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-red-500">
+      <Text className="text-white text-xl font-bold">
+        Hello Tailwind in React Native!
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ babel.config.js
@@ -0,0 +1,8 @@
+// module.exports = {
+//   presets: ['module:@react-native/babel-preset'],
+// };
+
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['nativewind/babel'], // 👈 add this line
+};
